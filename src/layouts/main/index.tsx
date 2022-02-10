0e6d66bfb6e5f6b5524b9ccec8c98502a0678f39
Sebastian <sebastian.luque.pl@gmail.com>
2022-02-09 20:25:24 -0500

    add logout button

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,9 +1,19 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 const MainLayout = ({ children }) => {
+  const router = useRouter()
+
+  const onLogout = () => {
+    localStorage.removeItem('session')
+
+    router.push('/')
+  }
+
   return (
     <Flex minH="100vh" direction="column">
       <Box mx="auto" flex={1} p={4} maxW={'7xl'} width="100%">
+        <Button onClick={onLogout}>Cerrar sesión</Button>
         {children}
       </Box>
     </Flex>
